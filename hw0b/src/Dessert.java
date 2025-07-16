26135d8f4319ab83c35b45d5e8d389b1175d3453
boluomi <635237669@qq.com>
2025-07-16 20:51:37 +0800

    great

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    public int flavor;
+    public int price;
+    public static int numDesserts = 0;
+
+    public Dessert(int w,int v){
+        flavor=w;
+        price=v;
+        numDesserts++;
+    }
+
+    public void printDessert(){
+        System.out.println(flavor+" "+price+" "+numDesserts);
+    }
+
+    public static void main(String[] args){
+        System.out.println("I love dessert!");
+    }
 }
